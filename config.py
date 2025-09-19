1c0466d2b98653e14f3827c4c1b505a49b833be8
Nelson <nelsonr9608@nauta.cu>
2025-09-19 18:17:23 -0400

    Sincronización automática desde Termux

--- config.py
+++ config.py
@@ -4,6 +4,6 @@ class Config:
     API_ID = int(os.environ.get("API_ID"))
     API_HASH = os.environ.get("API_HASH")
     BOT_TOKEN = os.environ.get("BOT_TOKEN")
-    LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL"))  # ID de canal privado para logs
-    ADMINS = [int(x) for x in os.environ.get("ADMINS", "").split()]  # IDs de admins
+    LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL"))  # ID canal privado para logs
+    ADMINS = [int(x) for x in os.environ.get("ADMINS", "").split()]  # IDs admins
     EXPIRATION_HOURS = int(os.environ.get("EXPIRATION_HOURS", 4))
